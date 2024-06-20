612281bd31fe00cbd7e9da5a86441002606e2390
abetoluwani <101352514+abetoluwani@users.noreply.github.com>
2024-06-20 15:16:20 -0400

    chat 75% completed

--- lib/app/modules/chat/views/chat_view.dart
+++ lib/app/modules/chat/views/chat_view.dart
@@ -1,22 +1,50 @@
+import 'package:flaury_provider/app/widgets/textfields.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:get/get.dart';
 
+import '../../../utils/constants/images.dart';
+import '../../../utils/theme/colors.dart';
+import '../../../widgets/apptext.dart';
+import '../../../widgets/space.dart';
 import '../controllers/chat_controller.dart';
+import 'widgets/chatwidget.dart';
 
 class ChatView extends GetView<ChatController> {
-  const ChatView({Key? key}) : super(key: key);
+  const ChatView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ChatView'),
-        centerTitle: true,
+        title: BigAppText('Chat'),
       ),
-      body: const Center(
-        child: Text(
-          'ChatView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: simPad(15, 15),
+              child: const AppTextFormField(
+                  textInputAction: TextInputAction.next,
+                  hint: 'Search',
+                  prefixIcon: Icon(Icons.search)),
+            ),
+            vSpace(20),
+            Expanded(
+              flex: 2,
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return const ChatWidget(
+                    title: 'Pella Tehilah',
+                    subtitle: 'Hello, how are you',
+                    image: AppImages.profileimg,
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/app/modules/chat/views/widgets/chatwidget.dart
@@ -0,0 +1,109 @@
+import 'package:flaury_provider/app/utils/constants/sizes.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+
+import '../../../../utils/theme/colors.dart';
+import '../../../../widgets/apptext.dart';
+import '../../../../widgets/space.dart';
+
+class ChatWidget extends StatelessWidget {
+  const ChatWidget({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.image,
+  });
+
+  final String title;
+  final String subtitle;
+  final String image;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Handle tap gesture here (e.g., navigate to another page)
+        if (kDebugMode) {
+          print('Chat container tapped!');
+        }
+      },
+      child: Center(
+        child: Container(
+          padding: simPad(0, 20),
+          width: double.infinity,
+          height: 70.h,
+          decoration: const BoxDecoration(
+            color: AppColors.white,
+          ),
+          // Inner container with the icon
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 50.w,
+                height: 50.h,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.transparent,
+                  image: DecorationImage(
+                      image: AssetImage(image), fit: BoxFit.cover),
+                ),
+              ),
+              hSpace(AppSizes.md),
+              // header text and description
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 220.h,
+                    child: BigAppText(
+                      title,
+                      fontSize: 20,
+                    ),
+                  ),
+                  vSpace(5),
+                  SizedBox(
+                    width: 220.h,
+                    child: SmallAppText(
+                      subtitle,
+                      color: AppColors.grey,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  // Message count
+                  CircleAvatar(
+                      backgroundColor: AppColors.primary,
+                      radius: 10,
+                      child: Center(
+                        child: SmallAppText(
+                          '2',
+                          color: AppColors.white,
+                          fontSize: 12,
+                        ),
+                      )),
+                  vSpace(AppSizes.sm),
+                  // Time
+                  SmallAppText(
+                    '11:00 AM',
+                    color: AppColors.grey,
+                    fontSize: 15,
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
